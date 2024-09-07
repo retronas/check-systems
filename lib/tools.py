35b44c6e3c6960b45f0ef83bf1a4326a0bf8d568
sairuk <sairuk@gmail.com>
2024-09-08 09:05:19 +1000

    add check for incomplete dataset

--- lib/tools.py
+++ lib/tools.py
@@ -24,9 +24,15 @@ class Tools():
             self.logger.log_info("No missing %s systems found" % left_name)
 
     def compare(self, left, right, left_name="", right_name="", ignore=[]):
-        self.logger.log_info('Running left (%s) > right (%s) compare' % (left_name, right_name))
-        self.diff(left, right, left_name, right_name, ignore)
+        if len(left) and len(right):
+            self.logger.log_info('Running left (%s) > right (%s) compare' % (left_name, right_name))
+            self.diff(left, right, left_name, right_name, ignore)
+        else:
+            self.logger.log_error("Data sets were incomplete, comparison is not possible")
 
     def inverse_compare(self, left, right, left_name="", right_name="", ignore=[]):
-        self.logger.log_info('Running right (%s) > left (%s) compare' % (right_name, left_name))
-        self.diff(right, left, right_name, left_name, ignore)
+        if len(left) and len(right):
+            self.logger.log_info('Running right (%s) > left (%s) compare' % (right_name, left_name))
+            self.diff(right, left, right_name, left_name, ignore)
+        else:
+            self.logger.log_error("Data sets were incomplete, inverse comparison is not possible")
